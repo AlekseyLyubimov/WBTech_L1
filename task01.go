4f07c5c5fdfd2a2feed81ac0a4d55c24d7f4388b
AlekseyLyubimov <lyubimov.aleksey.92@gmail.com>
2023-12-21 16:02:04 +0600

    task 1

---
+++ task01.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func task01() {
+	println("Task 1:")
+	Action{Human{name: "Aleksey", age: 31}, "demo action"}.Greetings()
+}
+
+type Human struct {
+	age    int
+	name   string
+}
+
+type Action struct {
+	Human
+	actionName string
+}
+
+func (human Human) Greetings() {
+	fmt.Printf("Hi, my name is %s, I am %d years ol.\n", human.name, human.age)
+}
