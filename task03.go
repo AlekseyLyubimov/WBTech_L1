806e5f239c00108fd1b9db1fe95eee0c43ff7b17
AlekseyLyubimov <lyubimov.aleksey.92@gmail.com>
2023-12-21 21:32:08 +0600

    task 3

--- task03.go
+++ task03.go
@@ -1,38 +1,23 @@
 package main
 
-import (
-	"sync"
-)
-
 func task03() {
 	numbers := []int{2, 4, 6, 8, 10}
-
-	wg := sync.WaitGroup{}
-	main_ch := make(chan int)
-	wg.Add(1)
-	recurrent_sum_sqr(numbers, main_ch, &wg)
-
-	wg.Wait()
-	x := <-main_ch
-	println(x)
+	ch := recurrent_sum_sqr(numbers)
+	println(<-ch)
 }
 
-func recurrent_sum_sqr(input []int, channel chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
+func recurrent_sum_sqr(input []int) <-chan int {
+	ch := make(chan int)
+	go func() {
+		if len(input) == 1 {
+			ch <- (input[0] * input[0])
+		} else {
+			ch1 := recurrent_sum_sqr(input[:len(input)/2])
+			ch2 := recurrent_sum_sqr(input[len(input)/2:])
+			x, y := <-ch1, <-ch2
+			ch <- x + y
+		}
+	}()
 
-	if len(input) == 1 {
-		channel <- (input[0] * input[0])
-	} else {
-		ch1 := make(chan int)
-		ch2 := make(chan int)
-
-		wg.Add(2)
-		recurrent_sum_sqr(input[:len(input)/2], ch1, wg)
-		recurrent_sum_sqr(input[len(input)/2:], ch2, wg)
-
-		x, y := <-ch1, <-ch2
-
-		channel <- x + y
-	}
-	close(channel)
+	return ch
 }
