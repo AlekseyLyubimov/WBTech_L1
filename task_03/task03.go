3ae260a591f9166427d7808b17a0debd7d9af33d
AlekseyLyubimov <lyubimov.aleksey.92@gmail.com>
2023-12-22 14:29:41 +0600

    alternative task 3 solution

--- task_03/task03.go
+++ task_03/task03.go
@@ -2,6 +2,35 @@ package main
 
 func main() {
 	numbers := []int{2, 4, 6, 8, 10}
+
+	main_ch := make(chan int)
+	go recurrent_sum_sqr(numbers, main_ch)
+
+	x := <-main_ch
+	println(x)
+}
+
+func recurrent_sum_sqr(input []int, channel chan int) {
+	if len(input) == 1 {
+		channel <- (input[0] * input[0])
+	} else {
+		ch1 := make(chan int)
+		ch2 := make(chan int)
+
+		go recurrent_sum_sqr(input[:len(input)/2], ch1)
+		go recurrent_sum_sqr(input[len(input)/2:], ch2)
+
+		x, y := <-ch1, <-ch2
+		channel <- x + y
+	}
+}
+
+/*
+
+alternative implementation
+
+func main() {
+	numbers := []int{2, 4, 6, 8, 10}
 	ch := recurrent_sum_sqr(numbers)
 	println(<-ch)
 }
@@ -21,3 +50,4 @@ func recurrent_sum_sqr(input []int) <-chan int {
 
 	return ch
 }
+*/
