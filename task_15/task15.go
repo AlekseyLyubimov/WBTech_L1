c0cbf633044bf7f7adf0cc7d1ec6783f23cddcb9
AlekseyLyubimov <lyubimov.aleksey.92@gmail.com>
2023-12-25 12:36:56 +0600

    task 15

--- task_15/task15.go
+++ task_15/task15.go
@@ -1,19 +1,34 @@
 package main
 
+import "strings"
+
 //К каким негативным последствиям может привести данный фрагмент кода, и как это исправить? Приведите корректный пример реализации.
 /*
 	няп проблема в том, что ХугеСтринг целиком останется в памяти, потому что слайс на неё ссылается, и вместо 100 килобайт будет занят 1 кибибайт
-	нужно использовать сделать копию слайса через strings.Clone(v[:100])
+	нужно сделать глубокую копию через strings.Clone(v[:100])
 */
 
-
-
 var justString string
+
+func someFunc_corrected() {
+  v := createHugeString(1 << 10)
+  justString = strings.Clone(v[:100])
+}
+
 func someFunc() {
   v := createHugeString(1 << 10)
   justString = v[:100]
 }
 
 func main() {
-  someFunc()
+  someFunc_corrected()
+  println(justString)
+}
+
+func createHugeString(i int) (result string) {
+	for i > 0 {
+		result += "W"
+		i--
+	}
+  return result
 }
